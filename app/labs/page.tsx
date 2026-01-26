f741670098fc5b83ee659c7ca988ef72b9c71144
kallat-a <kallat.a@northeastern.edu>
2026-01-25 22:52:16 -0500

    Add section number and Kambaz link to Labs page

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -4,7 +4,7 @@ export default function Labs() {
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
-      <p>Student Name: Ahaan Kallat</p>
+      <p>Student Name: Ahaan Kallat, Section: CS4550.33211.202630</p>
       <ul>
         <li>
           <Link href="/labs/lab1" id="wd-lab1-link">
@@ -22,6 +22,9 @@ export default function Labs() {
           </Link>
         </li>
       </ul>
+      <p>
+        <Link href="/" id="wd-kambaz-link-from-labs">Kambaz Application</Link>
+      </p>
       <p>
         <a href="https://github.com/kallat-a/kambaz-next-js" id="wd-github">
           GitHub Repository
