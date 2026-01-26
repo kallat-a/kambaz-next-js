f8c82eea063e125b0b515b589d1c63c21369886b
kallat-a <kallat.a@northeastern.edu>
2026-01-25 23:20:03 -0500

    Add type number to points

--- app/(kambaz)/courses/[cid]/assignments/[aid]/page.tsx
+++ app/(kambaz)/courses/[cid]/assignments/[aid]/page.tsx
@@ -19,7 +19,7 @@ export default function AssignmentEditor() {
               <label htmlFor="wd-points">Points</label>
             </td>
             <td>
-              <input id="wd-points" defaultValue={100} />
+              <input type="number" id="wd-points" defaultValue={100} />
             </td>
           </tr>
           <tr>
