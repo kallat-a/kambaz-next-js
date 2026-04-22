b42996b9cb7786fee4ae3098af9d58ce166239c6
kallat-a <kallat.a@northeastern.edu>
2026-04-22 18:15:53 -0400

    Load user enrollments on session bootstrap for deep links.

--- app/(kambaz)/Session.tsx
+++ app/(kambaz)/Session.tsx
@@ -4,6 +4,8 @@ import { ReactNode, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import * as client from "./account/client";
 import { setCurrentUser } from "./account/reducer";
+import { setEnrollments } from "./enrollments/reducer";
+import * as enrollmentsClient from "./enrollments/client";
 
 export default function Session({ children }: { children: ReactNode }) {
   const dispatch = useDispatch();
@@ -13,8 +15,21 @@ export default function Session({ children }: { children: ReactNode }) {
       try {
         const user = await client.profile();
         if (!cancelled) dispatch(setCurrentUser(user));
+        if (!cancelled && user?._id) {
+          try {
+            const list = await enrollmentsClient.findEnrollmentsForUser(
+              user._id,
+            );
+            if (!cancelled) dispatch(setEnrollments(list));
+          } catch {
+            if (!cancelled) dispatch(setEnrollments([]));
+          }
+        } else if (!cancelled) {
+          dispatch(setEnrollments([]));
+        }
       } catch {
         if (!cancelled) dispatch(setCurrentUser(null));
+        if (!cancelled) dispatch(setEnrollments([]));
       }
     })();
     return () => {
